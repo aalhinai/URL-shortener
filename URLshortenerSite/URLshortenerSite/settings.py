9a1aafd57091cc6e1378ab411783d696b24bb6fd
Abdullah <aalhinai83@gmail.com>
2016-12-12 14:41:25 +0100

    change PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__)) in settings

--- URLshortenerSite/URLshortenerSite/settings.py
+++ URLshortenerSite/URLshortenerSite/settings.py
@@ -20,7 +20,8 @@ import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+#PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATICFILES_DIRS = (os.path.join(PROJECT_ROOT,'static'),)
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
