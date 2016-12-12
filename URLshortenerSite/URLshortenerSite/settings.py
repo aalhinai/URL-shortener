925faf938e450403688f568b47b03a832432a2f6
Abdullah <aalhinai83@gmail.com>
2016-12-12 14:22:18 +0100

    Add STATICFILES_DIRS to settings

--- URLshortenerSite/URLshortenerSite/settings.py
+++ URLshortenerSite/URLshortenerSite/settings.py
@@ -21,7 +21,7 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-#STATICFILES_DIRS = (os.path.join(PROJECT_ROOT,'static'),)
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT,'static'),)
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 
